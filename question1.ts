007055dd224708d7c5dc29bc151861d2f85d501d
HatemMuhamad <hatem@vividtheory.com>
2022-08-17 09:48:01 -0600

    Adding func3

--- question1.ts
+++ question1.ts
@@ -1,6 +1,6 @@
 //Question 1
 
-// first func
+// first func: f: A -> [A] -> [A]
 const addItem = <A>(a: A, l: Array<A>): Array<A> => {
   l[l.length] = a;
   return l;
@@ -9,8 +9,29 @@ const filter = <A>(a: A, l: Array<A>): Array<A> => {
   return l.filter((num) => num != a);
 };
 
-// second func
+// second func: f: Number -> Number -> [A] -> [A]
 const slice = <A>(x: number, y: number, l: Array<A>): Array<A> => l.slice(x, y);
+
+// third function: f: [String] -> {String: any} -> {String: any}
+type MyObject = {
+  [key: string]: any;
+};
+const getValues = (l: Array<string>, obj: MyObject): MyObject => {
+  return Object.keys(obj)
+    .filter((key: string) => l.includes(key))
+    .reduce((newObj: MyObject, key: string) => {
+      newObj[key] = obj[key];
+      return newObj;
+    }, {});
+};
+
+const anObj: MyObject = {
+  a: 123,
+  b: 345,
+  c: 678,
+};
+const aList: Array<string> = ["a", "b", "c"];
+console.log(getValues(aList, anObj));
 
 // fourth func
 const zipperList = <A, B>(a: Array<A>, b: Array<B>): [A, B][] => {
